dec716b114619d352939c7ad3007e7a46f27619d
MUSAB ALQARNI <78758932+MusabSwe@users.noreply.github.com>
2022-10-20 18:54:01 +0300

    TypeScript with Attays, Enum, Tuples

--- Developing with TypeScript/build/index.js
+++ Developing with TypeScript/build/index.js
@@ -15,3 +15,11 @@ multiply(a, d);
 add(a, b);
 add(a, c);
 add(a, d);
+// Arrays examples in TS
+var myArr1 = [1, 2, 3];
+// Arrays withe different values of different types
+var arr = [1, 2, 3, 'a'];
+console.log('Different type array: ', arr);
+console.log('Same type array: ', myArr1);
+// Tuples  Ex
+var student = ['Ali', 30, 'Managment', 'Business school'];
--- Developing with TypeScript/src/index.ts
+++ Developing with TypeScript/src/index.ts
@@ -18,3 +18,18 @@ multiply(a, d);
 add(a, b)
 add(a, c)
 add(a, d)
+// Arrays examples in TS
+const myArr1: number[] = [1, 2, 3];
+// Arrays withe different values of different types
+const arr: (number | string)[] = [1, 2, 3, 'a'];
+console.log('Different type array: ', arr)
+console.log('Same type array: ', myArr1);
+// Tuples  Ex
+const student: [string, number, string, string] = ['Ali', 30, 'Managment', 'Business school']
+console.log(student);
+// Enum
+enum Compass { North = "North", South = "South", East = "East", West = "West" };
+const move = (dist: number, direction: Compass) => {
+    return `walk ${dist} paces ${direction}`;
+}
+console.log(move(10, Compass.East));
