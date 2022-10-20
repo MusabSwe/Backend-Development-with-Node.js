d4cd5cf1dbf466ebb547085bccf7bcabe0c37bfd
MUSAB ALQARNI <78758932+MusabSwe@users.noreply.github.com>
2022-10-20 20:02:25 +0300

    Excercise convert JavaScript to TypeScript

--- Developing with TypeScript/build/index.js
+++ Developing with TypeScript/build/index.js
@@ -1,25 +1,51 @@
 "use strict";
-var a = 4;
-var b = 6;
-var c = '5';
-var d = 'cat';
-var multiply = function (num1, num2) {
-    console.log("".concat(num1, " * ").concat(num2, " ="), num1 * num2);
-};
-var add = function (num1, num2) {
-    console.log("".concat(num1, " + ").concat(num2, " ="), num1 * num2);
-};
-multiply(a, b);
-multiply(a, c);
-multiply(a, d);
-add(a, b);
-add(a, c);
-add(a, d);
-// Arrays examples in TS
-var myArr1 = [1, 2, 3];
-// Arrays withe different values of different types
-var arr = [1, 2, 3, 'a'];
-console.log('Different type array: ', arr);
-console.log('Same type array: ', myArr1);
-// Tuples  Ex
-var student = ['Ali', 30, 'Managment', 'Business school'];
+var arrays = require('./utilities/arrays');
+var numbers = require('./utilities/numbers');
+var strings = require('./utilities/strings');
+// import arrays from "./utilities/arrays";
+// import numbers from './utilities/numbers'; Nodejs use commonjs in the modules and commonjs use require& module.export
+// import strings from './utilities/strings'; However ES6 use import export
+// const a = 4;
+// const b = 6;
+// const c = ('5' as unknown) as number;
+// const d = ('cat' as unknown) as number;
+// const multiply = (num1: number, num2: number) => {
+//     console.log(`${num1} * ${num2} =`, num1 * num2);
+// }
+// const add = (num1: number, num2: number) => {
+//     console.log(`${num1} + ${num2} =`, num1 * num2);
+// }
+// multiply(a, b);
+// multiply(a, c);
+// multiply(a, d);
+// add(a, b)
+// add(a, c)
+// add(a, d)
+// // Arrays examples in TS
+// const myArr1: number[] = [1, 2, 3];
+// // Arrays withe different values of different types
+// const arr: (number | string)[] = [1, 2, 3, 'a'];
+// console.log('Different type array: ', arr)
+// console.log('Same type array: ', myArr1);
+// // Tuples  Ex
+// const student: [string, number, string, string] = ['Ali', 30, 'Managment', 'Business school']
+// console.log(student);
+// // Enum
+// enum Compass { North = "North", South = "South", East = "East", West = "West" };
+// const move = (dist: number, direction: Compass) => {
+//     return `walk ${dist} paces ${direction}`;
+// }
+// console.log(move(10, Compass.East));
+// NOTE: This code has not been converted to TypeScript yet
+// Task convert JavaScript code to TypeScript code
+var numArr = [3, 4, 5, 6];
+var wordArr = ['cat', 'dog', 'rabbit', 'bird'];
+var arrSum = arrays.addArr(numArr);
+var mixArr = arrays.concatArr(numArr, wordArr);
+var myNum = '15' % 2;
+// results of function calls
+console.log(arrays.cut3(mixArr));
+console.log(numbers.sum(arrSum, myNum));
+console.log(strings.capitalize('the quick brown fox'));
+console.log(numbers.multiply('5', 8));
+console.log(arrays.lgNum(mixArr));
--- Developing with TypeScript/src/index.ts
+++ Developing with TypeScript/src/index.ts
@@ -1,35 +1,60 @@
-const a = 4;
-const b = 6;
-const c = ('5' as unknown) as number;
-const d = ('cat' as unknown) as number;
+const arrays = require('./utilities/arrays');
+const numbers = require('./utilities/numbers');
+const strings = require('./utilities/strings');
+// import arrays from "./utilities/arrays";
+// import numbers from './utilities/numbers'; Nodejs use commonjs in the modules and commonjs use require& module.export
+// import strings from './utilities/strings'; However ES6 use import export
 
-const multiply = (num1: number, num2: number) => {
-    console.log(`${num1} * ${num2} =`, num1 * num2);
-}
 
-const add = (num1: number, num2: number) => {
-    console.log(`${num1} + ${num2} =`, num1 * num2);
-}
+// const a = 4;
+// const b = 6;
+// const c = ('5' as unknown) as number;
+// const d = ('cat' as unknown) as number;
 
-multiply(a, b);
-multiply(a, c);
-multiply(a, d);
+// const multiply = (num1: number, num2: number) => {
+//     console.log(`${num1} * ${num2} =`, num1 * num2);
+// }
 
-add(a, b)
-add(a, c)
-add(a, d)
-// Arrays examples in TS
-const myArr1: number[] = [1, 2, 3];
-// Arrays withe different values of different types
-const arr: (number | string)[] = [1, 2, 3, 'a'];
-console.log('Different type array: ', arr)
-console.log('Same type array: ', myArr1);
-// Tuples  Ex
-const student: [string, number, string, string] = ['Ali', 30, 'Managment', 'Business school']
-console.log(student);
-// Enum
-enum Compass { North = "North", South = "South", East = "East", West = "West" };
-const move = (dist: number, direction: Compass) => {
-    return `walk ${dist} paces ${direction}`;
-}
-console.log(move(10, Compass.East));
+// const add = (num1: number, num2: number) => {
+//     console.log(`${num1} + ${num2} =`, num1 * num2);
+// }
+
+// multiply(a, b);
+// multiply(a, c);
+// multiply(a, d);
+
+// add(a, b)
+// add(a, c)
+// add(a, d)
+// // Arrays examples in TS
+// const myArr1: number[] = [1, 2, 3];
+// // Arrays withe different values of different types
+// const arr: (number | string)[] = [1, 2, 3, 'a'];
+// console.log('Different type array: ', arr)
+// console.log('Same type array: ', myArr1);
+// // Tuples  Ex
+// const student: [string, number, string, string] = ['Ali', 30, 'Managment', 'Business school']
+// console.log(student);
+// // Enum
+// enum Compass { North = "North", South = "South", East = "East", West = "West" };
+// const move = (dist: number, direction: Compass) => {
+//     return `walk ${dist} paces ${direction}`;
+// }
+// console.log(move(10, Compass.East));
+
+// NOTE: This code has not been converted to TypeScript yet
+// Task convert JavaScript code to TypeScript code
+
+
+const numArr: number[] = [3, 4, 5, 6];
+const wordArr: string[] = ['cat', 'dog', 'rabbit', 'bird'];
+const arrSum = arrays.addArr(numArr);
+const mixArr = arrays.concatArr(numArr, wordArr);
+const myNum = ('15' as unknown) as number % 2;
+
+// results of function calls
+console.log(arrays.cut3(mixArr));
+console.log(numbers.sum(arrSum, myNum));
+console.log(strings.capitalize('the quick brown fox'));
+console.log(numbers.multiply('5', 8));
+console.log(arrays.lgNum(mixArr));
